4ab0a4f0c61dd91c9fb68d21fc2852140119358a
Sosnowsky <juan.manuel.losada.sosnovsky@gmail.com>
2020-12-21 18:56:49 +0100

    Stuff

--- my_scripts/plotting/power_density.py
+++ my_scripts/plotting/power_density.py
@@ -1,50 +1,65 @@
 import matplotlib.pyplot as plt
 import numpy as np
-from scipy.optimize import curve_fit
 from scipy import signal
+from scipy.optimize import curve_fit
 
 
 def lineal(x, a, s):
     return a * x + s
 
 
-def plot_pds(x, dt, cutoff_min, cutoff_max):
-    fig, ax = plt.subplots(1, 2)
-    ax[0].axvline(cutoff_min, ls="dashed", alpha=0.5)
-    ax[0].axvline(cutoff_max, ls="dashed", alpha=0.5)
+def plot_pds(
+    x,
+    dt,
+    cutoff_min,
+    cutoff_max,
+    ax,
+    ax_product=None,
+    color_scatter="blue",
+    color_fit="red",
+    label="",
+    nperseg=None,
+):
+    ax.axvline(cutoff_min, ls="dashed", alpha=0.5)
+    ax.axvline(cutoff_max, ls="dashed", alpha=0.5)
 
-    f, Pxx_den = signal.welch(x, fs=1 / dt, nperseg=2 ** 20)
-    ax[0].scatter(f, Pxx_den)
+    if nperseg is None:
+        nperseg = len(x) / 16
+    f, Pxx_den = signal.welch(x, fs=1 / dt, nperseg=nperseg)
+    ax.scatter(f, Pxx_den, color=color_scatter)
     popt, pcov = curve_fit(
         lineal,
         np.log10(f[np.logical_and(f > cutoff_min, f < cutoff_max)]),
         np.log10(Pxx_den[np.logical_and(f > cutoff_min, f < cutoff_max)]),
         maxfev=10000,
     )
-    ax[0].plot(f, 10 ** lineal(np.log10(f), *popt), "r-")
-
-    ax[0].set_ylabel(r"$\log(\Phi(f))$")
-    ax[0].set_xlabel(r"$\log(f)$")
-    ax[0].set_xscale("log")
-    ax[0].set_yscale("log")
-    ax[0].set_title(r"$\Phi(f)$")
-    ax[0].set_xlim(cutoff_min / 1e2, cutoff_max * 1e2)
-    ax[0].text(
-        0.6,
-        0.7,
-        r"$\beta \approx {:.2f}({:.2f})$".format(-popt[0], np.sqrt(pcov[0, 0])),
-        transform=ax[0].transAxes,
+    ax.plot(
+        f,
+        10 ** lineal(np.log10(f), *popt),
+        "r-",
+        label=r"{} $\beta \approx {:.2f}({:.2f})$".format(
+            label, -popt[0], np.sqrt(pcov[0, 0])
+        ),
+        color=color_fit,
     )
 
-    ax[1].axvline(cutoff_min, ls="dashed", alpha=0.5)
-    ax[1].axvline(cutoff_max, ls="dashed", alpha=0.5)
-    ax[1].scatter(f, np.power(f, -popt[0]) * Pxx_den)
-    ax[1].set_title(r"$\Phi(f) f ^ {\beta}$")  # .format(-popt[0]))
-    ax[1].set_xscale("log")
-    ax[1].set_yscale("log")
-    ax[1].set_xlabel(r"$\log(f)$")
-    ax[1].set_xlim(1e-5, 1e2)
-    ax[1].set_ylim(1, 1e3)
+    ax.set_ylabel(r"$\log(\Phi(f))$")
+    ax.set_xlabel(r"$\log(f)$")
+    ax.set_xscale("log")
+    ax.set_yscale("log")
+    ax.set_title(r"$\Phi(f)$")
+    ax.set_xlim(cutoff_min / 1e2, cutoff_max * 1e2)
+
+    if ax_product is not None:
+        ax_product.axvline(cutoff_min, ls="dashed", alpha=0.5)
+        ax_product.axvline(cutoff_max, ls="dashed", alpha=0.5)
+        ax_product.scatter(f, np.power(f, -popt[0]) * Pxx_den, color=color)
+        ax_product.set_title(r"$\Phi(f) f ^ {\beta}$")
+        ax_product.set_xscale("log")
+        ax_product.set_yscale("log")
+        ax_product.set_xlabel(r"$\log(f)$")
+        ax_product.set_xlim(1e-5, 1e2)
+        ax_product.set_ylim(1, 1e3)
 
     return ax
 
@@ -68,7 +83,7 @@ def plot_distribution(x):
         10 ** lineal(np.log10(x_axes), a=popt[0], s=popt[1]),
         "r-",
         color="blue",
-        )
+    )
     ax.text(
         0.3,
         0.7,
@@ -85,3 +100,73 @@ def plot_distribution(x):
     ax.set_xscale("log")
     ax.set_yscale("log")
     ax.set_title("mean {:.2g}".format(x.mean()))
+
+
+def plot_scaling(
+    x,
+    ax,
+    cutoff,
+    cutoff_min=None,
+    weights=None,
+    loglin=False,
+    num=50,
+    label="",
+    color="blue",
+):
+    if loglin:
+        x_axes = np.linspace(0, max(x) + 1, num=num)
+    else:
+        x_axes = np.floor(np.logspace(0, int(np.log10(max(x))) + 1, num=num))
+
+    y, x_axes = np.histogram(x, bins=x_axes, density=True, weights=weights)
+
+    if loglin:
+        x_axes = (x_axes[:-1] + x_axes[1:]) / 2
+    else:
+        x_axes = np.sqrt(x_axes[:-1] * x_axes[1:])
+
+    ax.scatter(x_axes[y != 0], y[y != 0], color=color)
+    perform_fit_over = np.logical_and(x_axes < cutoff, y > 0)
+    if not loglin:
+        ax.set_xscale("log")
+        if cutoff_min is not None:
+            perform_fit_over = np.logical_and(perform_fit_over, x_axes > cutoff_min)
+
+        if cutoff > min(x_axes) and cutoff < max(x_axes):
+            ax.axvline(cutoff, ls="dashed", alpha=0.5)
+
+        # Linear regresion
+        popt, pcov = curve_fit(
+            lineal,
+            np.log10(x_axes[perform_fit_over]),
+            np.log10(y[perform_fit_over]),
+            maxfev=10000,
+        )
+
+        ax.plot(
+            x_axes,
+            10 ** lineal(np.log10(x_axes), a=popt[0], s=popt[1]),
+            "r-",
+            color=color,
+            label=r"{} $\alpha \approx {:.2f}({:.2f})$".format(
+                label, -popt[0], np.sqrt(pcov[0, 0])
+            ),
+        )
+
+    if loglin:
+        popt, pcov = curve_fit(
+            lineal,
+            x_axes[perform_fit_over],
+            np.log10(y[perform_fit_over]),
+            maxfev=10000,
+        )
+
+        ax.plot(
+            x_axes,
+            10 ** lineal(x_axes, *popt),
+            "r-",
+            color="blue",
+            label=r"{} $\alpha \approx {:.2f}$".format(label, -popt[0]),
+        )
+
+    ax.set_yscale("log")
